e549f00c9990d079a69adf7f96a70b43b65d45ac
o6scripts <fndf0583@gmail.com>
2026-08-18 20:22:04 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,7 +5,7 @@ end
 local BASE = 'https://raw.githubusercontent.com/o6Scripts/o6scripts/main/games/'
 
 local games = {
-    [830072163] = 'greedy-growers.lua',
+    [74102906764176] = 'greedy-growers.lua',
 }
 
 local file = games[game.PlaceId]
