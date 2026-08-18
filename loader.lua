094fb24dbc4e88181ff894c8a0ab11e864f3ed61
o6scripts <fndf0583@gmail.com>
2026-08-18 20:03:22 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,16 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
 
+
+local BASE = 'https://raw.githubusercontent.com/joustingmatch/Ouroboros/main/games/'
+
+local games = {
+    [9190691]    = 'greedy-growers',
+}   
+
+local file = games[game.CreatorId]
+if file then
+    task.wait(math.random())
+    loadstring(game:HttpGet(BASE .. file))()
+end
