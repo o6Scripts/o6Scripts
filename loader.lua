427424a5a9cea1f9e71d1bdb82755f2f94f1dfa6
o6scripts <fndf0583@gmail.com>
2026-08-20 04:17:24 +0200

    Add criminality.lua to games list

--- loader.lua
+++ loader.lua
@@ -7,6 +7,7 @@ local BASE = 'https://raw.githubusercontent.com/o6Scripts/o6scripts/main/games/'
 local games = {
     [74102906764176] = 'greedy-growers.lua',
     [114697347887839] = 'monkey-escape.lua',
+    [4588604953] = 'criminality.lua',
 }
 
 local file = games[game.PlaceId]
