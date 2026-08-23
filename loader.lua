561aa9ab251161d0cc6c48f2c05b4e08fc5116ce
o6scripts <fndf0583@gmail.com>
2026-08-23 17:51:17 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,8 +2,15 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-local BASE = 'https://raw.githubusercontent.com/o6Scripts/o6scripts/main/games/'
+local BASE = 'https://raw.githubusercontent.com/o6Scripts/o6scripts/main/'
 
+-- 1. Lade deine UI-Library aus der uiloader.lua
+local Library = loadstring(game:HttpGet(BASE .. 'uiloader.lua'))()
+
+-- 2. Erstelle das Hauptfenster (passe den Namen hier nach Wunsch an)
+local Window = Library:CreateWindow("o6Scripts Hub")
+
+-- 3. Spiele-Tabelle
 local games = {
     [74102906764176] = 'greedy-growers.lua',
     [114697347887839] = 'monkey-escape.lua',
@@ -13,5 +20,9 @@ local games = {
 local file = games[game.PlaceId]
 if file then
     task.wait(math.random())
-    loadstring(game:HttpGet(BASE .. file))()
+    -- 4. Lade das Spiel-Skript und übergebe das Fenster
+    local gameScript = loadstring(game:HttpGet(BASE .. 'games/' .. file))()
+    if type(gameScript) == "function" then
+        gameScript(Window)
+    end
 end
