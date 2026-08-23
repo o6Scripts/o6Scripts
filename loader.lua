446461740f9dbceabbc2c171cc815bf5d08a306e
o6scripts <fndf0583@gmail.com>
2026-08-23 18:44:17 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,12 +1,18 @@
 --!nocheck
+--[[
+    Loader - Game Detection + Script Loader
+    1. Detects game by PlaceId
+    2. Loads Compkiller library
+    3. Loads game script
+]]
 
 local GITHUB_URL = "https://raw.githubusercontent.com/o6Scripts/o6Scripts/main"
 
 local GAMES = {
-    [74102906764176] = "games/greedy-growers.lua",
+    ["74102906764176"] = "games/greedy-growers.lua",
 }
 
-local placeId = game.PlaceId
+local placeId = tostring(game.PlaceId)
 local scriptPath = GAMES[placeId]
 
 if not scriptPath then
@@ -22,7 +28,7 @@ local function fetch(path)
     return nil
 end
 
-
+-- 1. Load Compkiller library
 local libSrc = fetch("uiloader.lua")
 if not libSrc then
     warn("[Loader] Failed to fetch uiloader.lua")
@@ -30,7 +36,7 @@ if not libSrc then
 end
 loadstring(libSrc)()
 
-
+-- 2. Load game script (has access to global Compkiller)
 local gameSrc = fetch(scriptPath)
 if gameSrc then
     loadstring(gameSrc)()
