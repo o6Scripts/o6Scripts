526ff18d6f80c167131cc6e6d267564f93a59f2f
o6scripts <fndf0583@gmail.com>
2026-08-25 22:15:44 +0200

    Load game script based on CreatorId

--- loader.lua
+++ loader.lua
@@ -1,1 +1,14 @@
-a
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local BASE = 'https://raw.githubusercontent.com/o6Scripts/o6Scripts/main/games/'
+
+local games = {
+    [830072163] = 'greedygrowers.lua',
+}
+
+local file = games[game.CreatorId]
+if file then
+    loadstring(game:HttpGet(BASE .. file))()
+end
